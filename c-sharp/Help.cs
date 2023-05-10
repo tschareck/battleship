6fa18c769c179527890ff05cadcac531f6459d43
Aleksander P <tschareck@users.noreply.github.com>
2023-05-10 09:03:42 +0000

    Create project in C#

---
+++ c-sharp/Help.cs
@@ -0,0 +1,9 @@
+public static class Help{
+    public static void DisplayHelp ()
+    {
+        Console.WriteLine ("* To display help, type \"help\"");
+        Console.WriteLine ("* To finish, type \"exit\"");
+        Console.WriteLine ("* To start new game, type \"new\"");
+        Console.WriteLine ("* To shoot, type coordinate as: \"A10\"");
+    }
+}
---
+++ c-sharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+  static void Main ()
+  {
+    Help.DisplayHelp ();
+    string input = "";
+    while (input != "exit")
+    {
+        Console.Write ("Enter command: ");
+        input = Console.ReadLine();
+        if (input == "help")
+        {
+            Help.DisplayHelp ();
+        }
+        else if (input != "exit")
+        {
+            SomeMethod (input);
+        }
+    }
+  }
+
+  static void SomeMethod (string input)
+  {
+    Console.WriteLine ("You entered: " + input);
+  }
+
+}
